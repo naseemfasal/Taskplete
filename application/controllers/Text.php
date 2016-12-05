8a89d31e9ed1b07628cee7476386c758fc42dfa8
vmr007 <vishnuchemmeri@gmail.com>
2016-12-05 16:34:40 +0530

    VIshnu Test FIle

---
+++ application/controllers/Text.php
@@ -0,0 +1,14 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Text extends CI_Controller {
+
+    public function index()
+    {
+        echo "This is by vishnu";
+    }
+
+}
+
+/* End of file Text.php */

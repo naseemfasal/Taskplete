009ef9a25de696f9490f78a10b00caf256e90524
Naseem Fasal <naseem770@gmail.com>
2016-12-05 12:13:08 +0530

    Install.php

---
+++ application/controllers/install.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$this->db->query('CREATE TABLE tb_users
+(
+user_id INT NOT NULL AUTO_INCREMENT,
+PRIMARY KEY(user_id),
+name VARCHAR(50),
+email VARCHAR(50),
+password VARCHAR(50),
+activation_code VARCHAR(50),
+forgotten_password_code varchar(40),
+forgotten_password_time DATETIME,
+profile_id INT,
+created_on DATETIME,
+is_active TINYINT,
+)');
+
+
+?>

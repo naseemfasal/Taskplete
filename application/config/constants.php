b117ab0f61bdf2b1a358c45bd4ca6579458d71ae
Naseem Fasal <naseem770@gmail.com>
2016-12-06 13:19:57 +0530

    S

--- application/config/constants.php
+++ application/config/constants.php
@@ -86,5 +86,5 @@ defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest auto
 
 
 
-define('URL','http://localhost/taskplete');
-define('CSS',(CSS).'http://localhost/taskplete');
+// define('URL','http://localhost/taskplete');
+// define('CSS',(CSS).'http://localhost/taskplete');
